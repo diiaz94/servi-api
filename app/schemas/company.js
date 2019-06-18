2df8c445787ce5021ae84126a44c07de1c51ee31
Pedro Diaz <pdiaz.btm@gmail.com>
2019-06-18 01:09:50 -0400

    update dependencies

--- app/schemas/company.js
+++ app/schemas/company.js
@@ -1,11 +1,40 @@
 const mongoose = require('mongoose');
 
 let companySchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: {
+        type: String,
+        required: true
+    },
+    identification: {
+        type: {
+            type: String,
+            enum: [
+                'J', 'V'
+            ],
+            default: 'V'
+        },
+        value: String
+    },
+    state: String,
+    city: String,
     description: String,
-    phones: String,
-    verified: { type: Boolean, default: false },
-    visible: { type: Boolean, default: true },
+    phones: [String],
+    website: String,
+    facebook: String,
+    twitter: String,
+    instagram: String,
+    linkedin: String,
+    youtube: String,
+    email: String,
+    bussines_hours: String,
+    verified: {
+        type: Boolean,
+        default: false
+    },
+    visible: {
+        type: Boolean,
+        default: true
+    },
     location: {
         address: String,
         latitude: String,
@@ -13,14 +42,21 @@ let companySchema = new mongoose.Schema({
     },
     thumbnail_image: String,
     avatar_image: String,
-    categories: [{ type: mongoose.Schema.ObjectId, ref: 'Category' }]
-}, {
-        timestamps: {
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
+    categories: [
+        {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Category'
         }
-    });
+    ],
+    owner_name: String,
+    owner_email: String
 
-companySchema.index({ '$**': 'text' });
+
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
 
 module.exports = companySchema
